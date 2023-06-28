9fb9a9bf9e4a3ac3abc1d99eb55b01cb0067c334
a-khils <akhilsomanmeg@gmail.com>
2023-06-28 12:17:28 +0530

    redux create and setuped

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,9 +1,14 @@
 import React from "react";
+import { UseSelector, useSelector } from "react-redux";
+
 import Post from "./Post/Post";
 import useStyles from "./styles";
 
 const Posts = () => {
+  const posts = useSelector((state) => state.Posts);
   const classes = useStyles();
+
+  console.log(posts);
   return (
     <>
       <h1>POsts</h1>
---
+++ client/src/reducers/posts.js
@@ -0,0 +1,11 @@
+export default (posts = [], action) => {
+  switch (action.type) {
+    case "FETCH_ALL":
+      return action.playload;
+    case "CREATE":
+      return posts;
+
+    default:
+      break;
+  }
+};
